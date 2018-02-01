65a09c2f14b8a1d3f3672da4210c33b0a91a0f7e
Min Kim <mk4555@nyu.edu>
2018-02-01 17:38:10 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,6 @@
 class CouponsController < ApplicationController
   def index
+    @coupons = Coupon.all
   end
 
   def new
@@ -7,7 +8,7 @@ class CouponsController < ApplicationController
 
   def show
   end
-  
+
   def create
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
     redirect_to coupon_path(@coupon)
